530d31f062341dfc876c7e14816acef2b0027a8f
eymrich <kuurstra.renato@gmail.com>
2024-04-19 17:32:22 +0100

    First commit, basic ue files!

---
+++ Source/NGJ24/NGJ24.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NGJ24.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NGJ24, "NGJ24" );
---
+++ Source/NGJ24Editor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class NGJ24EditorTarget : TargetRules
+{
+	public NGJ24EditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		ExtraModuleNames.Add("NGJ24");
+	}
+}
