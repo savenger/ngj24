a5090c564aa8ba1acc256c4028271a4e090003da
salvolannister <darksawyer@hotmail.it>
2024-04-20 14:20:54 +0200

    DamageableComponent - added files

---
+++ Source/NGJ24/Private/DamageableComponent.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageableComponent.h"
+
+// Sets default values for this component's properties
+UDamageableComponent::UDamageableComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDamageableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UDamageableComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UDamageableComponent::TakeDamage(int32 DamageAmount)
+{
+	
+	CurrentLife = FMath::Clamp(CurrentLife, 0, CurrentLife - DamageAmount);
+	
+	if (CurrentLife == 0)
+	{
+		// Print on screen player dead
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Player is dead"));
+	}
+}
+
---
+++ Source/NGJ24/Public/DamageableComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DamageableComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NGJ24_API UDamageableComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDamageableComponent();
+
+	//DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTOnActorDeath)
+
+	UPROPERTY(BlueprintReadWrite)
+		int32 MaxLife;
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+		int32 CurrentLife;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Function to call to apply damage
+	UFUNCTION(BlueprintCallable, Category = "CustomDamage")
+		void TakeDamage(int32 DamageAmount);
+
+	// Action that gets called when the actor dies
+	//UPROPERTY(BlueprintAssignable, Category = "CustomDamage")
+	//FTOnActorDeath OnDamageableDeath;
+
+		
+};
